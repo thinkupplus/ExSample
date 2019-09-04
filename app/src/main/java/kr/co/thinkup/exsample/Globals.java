63adbeb918820f0265095605b60f45a7fdd4db2d
yong <thinkupplus@gmail.com>
2019-09-04 10:14:30 +0900

    Init Code

--- app/src/main/java/kr/co/thinkup/exsample/Globals.java
+++ app/src/main/java/kr/co/thinkup/exsample/Globals.java
@@ -1,14 +1,19 @@
 package kr.co.thinkup.exsample;
 
 import android.app.Application;
+import android.util.Log;
 
 /**
  * 2019-09-04 by yh.Choi
  */
 public class Globals extends Application {
+    
+    private static final String TAG = "Globals";
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Log.d(TAG, "onCreate: ");
     }
 }
